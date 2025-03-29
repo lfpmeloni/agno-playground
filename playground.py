21ed8065b81ee948fdbde1bdff6e94d391397ff4
Felipe Meloni <felipemeloni@hotmail.com>
2025-03-29 18:14:08 +0100

    clean custom team definition

--- playground.py
+++ playground.py
@@ -1,31 +1,40 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-from agno.playground import Playground, serve_playground_app
 
-# --- CUSTOM AGENTS ---
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.playground import Playground, serve_playground_app
+from agno.storage.agent.sqlite import SqliteAgentStorage
+from agno.team.team import Team
+
+# Custom agents
 from agents.exp_web_agent import web_agent
 from agents.exp_finance_agent import finance_agent
 from agents.mkt_board_of_directors import create_board_agent
 from agents.mkt_bod_interests import create_bod_interests_agent
 from agents.mkt_content_generator import create_content_generator_agent
 
-# --- CUSTOM TEAMS ---
+# Custom teams
 from teams.marketing_team import marketing_team
-# --- END OF CUSTOM AGENTS/TEAMS ---
 
-agent_storage: str = "tmp/agents.db"
+# SQLite path
+agent_storage = "tmp/agents.db"
 
+# Playground app definition
 app = Playground(
     agents=[
-        web_agent, 
+        web_agent,
         finance_agent,
         create_board_agent(),
         create_bod_interests_agent(),
-        create_content_generator_agent(),
+        create_content_generator_agent()
+    ],
+    teams=[
         marketing_team
     ]
 ).get_app()
 
+# Allow CORS for external browser access
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -34,9 +43,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Health check route
 @app.get("/v1/playground/status")
 async def playground_status():
     return JSONResponse(content={"status": "ok"})
 
+# Start the app
 if __name__ == "__main__":
     serve_playground_app("playground:app", host="0.0.0.0", port=7777, reload=True)
